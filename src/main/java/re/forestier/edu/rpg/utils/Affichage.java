bbcbaa117ae7676eceed4663e72736b399425426
DonYanis <beldjilaliyanis@gmail.com>
2024-12-15 13:10:43 +0100

    refactor Affichage

--- src/main/java/re/forestier/edu/rpg/utils/Affichage.java
+++ src/main/java/re/forestier/edu/rpg/utils/Affichage.java
@@ -7,22 +7,35 @@ import re.forestier.edu.rpg.Player;
 
 public class Affichage {
 
-    private static final List<String> abilityOrder = Arrays.asList("DEF","VIS", "ATK", "CHA", "INT", "ALC");
+    private static final List<String> ABILITY_ORDER = Arrays.asList("DEF", "VIS", "ATK", "CHA", "INT", "ALC");
 
     public static String afficherJoueur(Player player) {
-        final StringBuilder finalString = new StringBuilder("Joueur " + player.getAvatarName() + " joué par " + player.getPlayerName());
-        finalString.append("\nNiveau : " + player.retrieveLevel() + " (XP totale : " + player.getXp() + ")");
-        finalString.append("\n\nCapacités :");
+        StringBuilder finalString = new StringBuilder();
 
-        for (String ability : abilityOrder) {
-            if (player.getAbilities().containsKey(ability)) {
-                finalString.append("\n   " + ability + " : " + player.getAbilities().get(ability));
-            }
-        }
-
-        finalString.append("\n\nInventaire :");
-        player.getInventory().forEach(item -> finalString.append("\n   " + item));
+        appendPlayerDetails(finalString, player);
+        appendAbilities(finalString, player);
+        appendInventory(finalString, player);
 
         return finalString.toString();
+    }
+
+
+    private static void appendPlayerDetails(StringBuilder finalString, Player player) {
+        finalString.append(String.format("Joueur %s joué par %s", player.getAvatarName(), player.getPlayerName()))
+                .append("\nNiveau : ").append(player.retrieveLevel())
+                .append(" (XP totale : ").append(player.getXp()).append(")\n\nCapacités :");
+    }
+
+
+    private static void appendAbilities(StringBuilder finalString, Player player) {
+        ABILITY_ORDER.stream()
+                .filter(ability -> player.getAbilities().containsKey(ability)) 
+                .forEach(ability -> finalString.append("\n   ").append(ability).append(" : ").append(player.getAbilities().get(ability)));
+    }
+
+
+    private static void appendInventory(StringBuilder finalString, Player player) {
+        finalString.append("\n\nInventaire :");
+        player.getInventory().forEach(item -> finalString.append("\n   ").append(item));
     }
 }
