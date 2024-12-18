2811b1387f6acd6977bfd58260e9d1608e853edd
DonYanis <beldjilaliyanis@gmail.com>
2024-12-18 21:24:31 +0100

    from sonar : remove useless lines

--- src/main/java/re/forestier/edu/Main.java
+++ src/main/java/re/forestier/edu/Main.java
@@ -10,8 +10,7 @@ public class Main {
         Player firstPlayer = new Player("Florian", "Ruzberg de Rivehaute", "DWARF", 200, new ArrayList<>(),5);
         UpdatePlayer.addMoney(firstPlayer, 400);
         UpdatePlayer.addXp(firstPlayer, 15);
-        //System.out.println(Affichage.printPlayer(firstPlayer));
-        System.out.println("------------------");
+
         UpdatePlayer.addXp(firstPlayer, 20);
         System.out.println(Display.printPlayer(firstPlayer));
         System.out.println(Display.printPlayerInMarkDown(firstPlayer));
